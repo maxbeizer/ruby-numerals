0ae95f8b4c2a83588e01401f5f25cc53e13cd9bd
Max Beizer <max.beizer@gmail.com>
2015-08-26 19:17:34 -0500

    Add 1 -> I

--- lib/ruby_numeral.rb
+++ lib/ruby_numeral.rb
@@ -1,2 +1,11 @@
 class RubyNumeral
+  attr_reader :string
+
+  def initialize(string)
+    @string = string
+  end
+
+  def convert
+    'I'
+  end
 end
--- test/ruby_numeral_test.rb
+++ test/ruby_numeral_test.rb
@@ -2,7 +2,7 @@ require_relative './test_helper'
 require_relative '../lib/ruby_numeral'
 
 class TestRubyNumeral < Minitest::Test
-  def test_truth
-    assert true
+  def test_to_roman_1
+    assert_equal RubyNumeral.new('1').convert, 'I'
   end
 end
